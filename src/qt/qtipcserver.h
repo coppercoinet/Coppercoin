f8bdc1697aa3b9e615db6b1a93bf0c06a4c8dc73
Coppercoin <31774199+coppercoinet@users.noreply.github.com>
2017-09-10 10:13:53 -0700

    Coppercoin Source

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Coppercoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CoppercoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
